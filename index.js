9246546c8d47aa19be1fc193817fe2a32ac2a7a1
touy <touya.ra@gmail.com>
2019-07-07 08:37:30 +0700

    replace new Buffer([0]) with Buffer.from([0])
     new Buffer([0]),it returns TypeError: Buffer is not a constructor

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var shift = require('stream-shift')
 
 var SIGNAL_FLUSH = (Buffer.from && Buffer.from !== Uint8Array.from)
   ? Buffer.from([0])
-  : new Buffer([0])
+  : Buffer.from([0])
 
 var onuncork = function(self, fn) {
   if (self._corked) self.once('uncork', fn)
